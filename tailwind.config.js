e631bf4521a56519e912d5293e96c141a4700c27
chrisfromhood <kristijan@cogentdeciphering.com>
2022-05-13 18:29:27 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    backgroundImage: {
+      'usa-map': "url(../img/USA.svg);"
+    },
+    fontFamily: {
+      baskervville: ['Baskervville', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
