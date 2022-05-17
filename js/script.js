51a4e2a31510397a6a1fcc30cd170f37ff12c8eb
chrisfromhood <kristijan@cogentdeciphering.com>
2022-05-17 15:35:28 +0200

    text bar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 import marquee from 'https://cdn.jsdelivr.net/npm/vanilla-marquee/dist/vanilla-marquee.js';
 
-
-const scrollLeft = new marquee(document.getElementById('marqueeLeft'), {
+const scrollText = new marquee(document.getElementById('marqueeText'), {
+  speed: 100,
+  gap: 0,
+  delayBeforeStart: 0,
   direction: 'left',
+  duplicated: true,
+  pauseOnHover: true,
 });
+
 
 const scrollRight = new marquee(document.getElementById('marqueeRight'), {
   direction: 'right',
+  speed: 100,
+  gap: 0,
+  delayBeforeStart: 0,
+  duplicated: true,
+  pauseOnHover: true,
+});
+
+const scrollLeft = new marquee(document.getElementById('marqueeLeft'), {
+  speed: 100,
+  gap: 0,
+  delayBeforeStart: 0,
+  direction: 'left',
+  duplicated: true,
+  pauseOnHover: true,
 });
