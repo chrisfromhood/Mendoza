45b4597bc565414e3ddc56be8b36f7e0e75847ed
chrisfromhood <kristijan@cogentdeciphering.com>
2022-05-17 19:18:40 +0200

    mq

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,30 @@ module.exports = {
       helveticaThin: ['helveticaThin', 'sans-serif'],
       helveticaBold: ['helveticaBold', 'sans-serif'],
     },
+
     extend: {},
+    screens: {
+      'xl': { 'max': '1336px' },
+      // => @media (max-width: 1320px) { ... }
+
+      'lg': { 'max': '1200px' },
+      // => @media (max-width: 1200px) { ... }
+
+      'md': { 'max': '904px' },
+      // => @media (max-width: 944px) { ... }
+
+      'tablet': { 'max': '850px' },
+      // => @media (max-width: 850px) { ... }
+
+      'ipad': { 'max': '704px' },
+      // => @media (max-width: 704px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      'mob': { 'max': '544px' },
+      // => @media (max-width: 544px) { ... }
+    }
   },
   plugins: [],
 }
